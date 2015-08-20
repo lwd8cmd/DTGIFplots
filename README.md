0b3605211d19c831761dc40666728a7d04f875d0
lwd8cmd <lauri.h2marik@gmail.com>
2015-08-20 09:25:26 +0200

    readme, plots for all attenuations

---
+++ README.md
@@ -0,0 +1,23 @@
+# GIF++ DT currents plots
+
+Load DT chambers data and make plots
+
+# Usage
+
+* Download this project (gif.py and directory data/)
+* Put data files in data/scan_nr/ (for example first scan in data/1/, second in data/2/, ...)
+* Add attenuations time information in gif.py
+
+These are at the beginning
+
+```python
+if scan == 1:
+	def getAttenuation(time):# map time to attenuation
+		if time > parser.parse("2015-08-13 20:35:00") \
+			and time < parser.parse("2015-08-13 20:37:59"):
+			return 46420
+```
+
+* Select scan_nr to analyze (change scan = 2) at the beginning of the file
+* run script "python gif.py"
+* plots are automatically saved in the same directory
--- gif.py
+++ gif.py
@@ -7,41 +7,10 @@ from matplotlib.dates import DateFormatter
 from os import walk
 import re
 
-scan = 2
+scan = 2# what scan to process (txt file in path data/scan/)
 
-timestamps = []
-currents = []
-voltages = []
-wire_strs = ["wire0", "wire1", "strip", "cathode"]
-superlayer_strs = ['phi1', 'theta', 'phi2']
-
-path = "txt/{}/".format(scan)
-_, _, files = walk(path).next()
-for file in files:
-	m = re.search("^(.+?)\.txt$", file)
-	if not m:# filename is not in the correct form: eg a.txt
-		continue
-	
-	# load data from file
-	with open(path + file) as fp:
-		for line_nr, line in enumerate(fp):
-			if line_nr == 0:
-				continue
-			else:
-				data = line.split()
-				timestamps.append(parser.parse(data[0]+' '+data[1]))
-				currents.append([float(i) for i in data[2::2]])
-				voltages.append([float(i) for i in data[3::2]])
-
-# use numpy arrays (easier to manipulate)
-currents = np.array(currents)
-voltages = np.array(voltages)
-timestamps = np.array(timestamps)
-
-if len(currents) == 0:
-	print("Input file not found in dir {}. Check scan nr in line 10".format(path))
-	exit()
-
+######################################################
+# attenuations info
 if scan == 1:
 	def getAttenuation(time):# map time to attenuation
 		if time > parser.parse("2015-08-13 20:35:00") \
@@ -114,6 +83,41 @@ elif scan == 3:
 			and time < parser.parse("2015-08-17 13:40:59"):
 			return 100
 		return 0
+		
+######################################################
+
+timestamps = []
+currents = []
+voltages = []
+wire_strs = ["wire0", "wire1", "strip", "cathode"]
+superlayer_strs = ['phi1', 'theta', 'phi2']
+
+path = "data/{}/".format(scan)
+_, _, files = walk(path).next()
+for file in files:
+	m = re.search("^(.+?)\.txt$", file)
+	if not m:# filename is not in the correct form: eg a.txt
+		continue
+	
+	# load data from file
+	with open(path + file) as fp:
+		for line_nr, line in enumerate(fp):
+			if line_nr == 0:
+				continue
+			else:
+				data = line.split()
+				timestamps.append(parser.parse(data[0]+' '+data[1]))
+				currents.append([float(i) for i in data[2::2]])
+				voltages.append([float(i) for i in data[3::2]])
+
+# use numpy arrays (easier to manipulate)
+currents = np.array(currents)
+voltages = np.array(voltages)
+timestamps = np.array(timestamps)
+
+if len(currents) == 0:
+	print("Input file not found in dir {}. Check scan nr in line 10".format(path))
+	exit()
 	
 attenuations = np.array(map(getAttenuation, timestamps))
 mask = attenuations > 0
@@ -152,87 +156,76 @@ if 1:# for every superlayer, layer, wire
 				
 				# save plot
 				plt.tight_layout()
-				filename = 'plots/{s}/scan{s}SL{sl}L{l}{w}.png'.format(s=scan, sl=superlayer, l=layer, w=wire_strs[wire])
+				filename = 'data/{s}/scan{s}SL{sl}L{l}{w}.png'.format(s=scan, sl=superlayer, l=layer, w=wire_strs[wire])
 				print("Saving {}".format(filename))
 				plt.savefig(filename, bbox_inches='tight')
 				plt.clf()
 				
 if 1:# 2d plot
-	# load data
-	attenuation = [10, 100, 46][scan-1]
-	mask = attenuations == attenuation
-	values = np.zeros((3,4,4), dtype=np.float)
-	for superlayer in [1,2,3]:
-		for layer in [1,2,3,4]:
-			for wire in [0,1,3]:
-				values[superlayer-1, layer-1, wire] = (currents[:,(superlayer-1)*16+(layer-1)*4+wire][mask]).mean()
-	
 	matplotlib.rcParams.update({'font.size': 22})
-	# plot 1: x=layer, y=layer
-	for wire in [0,1,3]:
-		fig, ax = plt.subplots()
-		fig.set_size_inches(6.0, 6.0)
-		im = ax.pcolor(values[:,:,wire], cmap='YlOrBr', edgecolor='black', linestyle='--', lw=1)
-		fig.colorbar(im)
-		
-		ax.xaxis.set(ticks=np.arange(0.5, 4), ticklabels=['L1', 'L2', 'L3', 'L4'])
-		ax.yaxis.set(ticks=np.arange(0.5, 3), ticklabels=['phi1', 'theta', 'phi2'])
 				
-		plt.title("\n{}".format(time), fontsize=16)
-		plt.suptitle(r'{w} current ($\mu A$) attenuation={a}'.format(w=wire_strs[wire], a=attenuation))
+	for attenuation_i, attenuation in enumerate(np.unique(attenuations)):
+		mask = attenuations == attenuation
+		# load data
+		values = np.zeros((3,4,4), dtype=np.float)
+		for superlayer in [1,2,3]:
+			for layer in [1,2,3,4]:
+				for wire in [0,1,3]:
+					values[superlayer-1, layer-1, wire] = (currents[:,(superlayer-1)*16+(layer-1)*4+wire][mask]).mean()
 		
-		plt.tight_layout()
-		filename = 'plots/{s}/scan{s}2d1{w}.png'.format(s=scan, w=wire_strs[wire])
-		print("Saving {}".format(filename))
-		plt.savefig(filename, bbox_inches='tight')
-		plt.clf()
-		
-	# plot 2: y=superlayer x layer
-	for wire in [0,1,3]:
-		fig, ax = plt.subplots()
-		fig.set_size_inches(6.0, 6.0)
-		im = ax.pcolor(values[:,:,wire].reshape((-1,1))[::-1], cmap='YlOrBr', edgecolor='black')
-		fig.colorbar(im)
-		
-		ax.xaxis.set_ticks([])
-		ax.yaxis.set(ticks=np.arange(0.5, 12), ticklabels=['phi1 L1', 'L2', 'L3', 'L4', 'theta L1', 'L2', 'L3', 'L4', 'phi2 L1', 'L2', 'L3', 'L4'][::-1])
-		
-		plt.title("\n{}".format(time), fontsize=16)
-		plt.suptitle(r'{w} current ($\mu A$) attenuation={a}'.format(w=wire_strs[wire], a=attenuation))
-		
-		plt.tight_layout()
-		filename = 'plots/{s}/scan{s}2d2{w}.png'.format(s=scan, w=wire_strs[wire])
-		print("Saving {}".format(filename))
-		plt.savefig(filename, bbox_inches='tight')
-		plt.clf()
-		
-	# plot 3: y=superlayer x layer, x=wire
-	if 1:
-		fig, ax = plt.subplots()
-		fig.set_size_inches(6.0, 6.0)
-		im = ax.pcolor(values[:,:,:2].reshape((-1,2))[::-1], cmap='YlOrBr', edgecolor='black')
-		fig.colorbar(im)
-		
-		ax.xaxis.set(ticks=np.arange(0.5, 2), ticklabels=wire_strs[:2])
-		ax.yaxis.set(ticks=np.arange(0.5, 12), ticklabels=['phi1 L1', 'L2', 'L3', 'L4', 'theta L1', 'L2', 'L3', 'L4', 'phi2 L1', 'L2', 'L3', 'L4'][::-1])
-				
-		plt.title("\n{}".format(time), fontsize=16)
-		plt.suptitle(r'Current ($\mu A$) attenuation={a}'.format(a=attenuation))
-		
-		plt.tight_layout()
-		filename = 'plots/{s}/scan{s}2d3.png'.format(s=scan, w=wire_strs[wire])
-		print("Saving {}".format(filename))
-		plt.savefig(filename, bbox_inches='tight')
-		plt.clf()
-
-if 0:# time plot
-	#mask = attenuations > -1
-	#plt.plot(timestamps[mask], currents.mean(1)[mask], '.')
-	#plt.plot(timestamps[mask], attenuations[mask], '.')
-	#plt.plot(timestamps[mask], voltages.mean(1)[mask], '.')
-	ys = currents.mean(1)
-	plt.plot(timestamps[1:], ys[1:]-ys[:-1])
-	#plt.yscale('log')
-	plt.gcf().axes[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
-	plt.gcf().autofmt_xdate()
-	plt.show()
+		# plot 1: x=layer, y=layer
+		for wire in [0,1,3]:
+			fig, ax = plt.subplots()
+			fig.set_size_inches(6.0, 6.0)
+			im = ax.pcolor(values[:,:,wire], cmap='YlOrBr', edgecolor='black', linestyle='--', lw=1)
+			fig.colorbar(im)
+			
+			ax.xaxis.set(ticks=np.arange(0.5, 4), ticklabels=['L1', 'L2', 'L3', 'L4'])
+			ax.yaxis.set(ticks=np.arange(0.5, 3), ticklabels=['phi1', 'theta', 'phi2'])
+					
+			plt.title("\n{}".format(time), fontsize=16)
+			plt.suptitle(r'{w} current ($\mu A$) attenuation={a}'.format(w=wire_strs[wire], a=attenuation))
+			
+			plt.tight_layout()
+			filename = 'data/{s}/scan{s}2d1{w}att{a}.png'.format(s=scan, w=wire_strs[wire], a=attenuation_i)
+			print("Saving {}".format(filename))
+			plt.savefig(filename, bbox_inches='tight')
+			plt.clf()
+			
+		# plot 2: y=superlayer x layer
+		for wire in [0,1,3]:
+			fig, ax = plt.subplots()
+			fig.set_size_inches(6.0, 6.0)
+			im = ax.pcolor(values[:,:,wire].reshape((-1,1))[::-1], cmap='YlOrBr', edgecolor='black')
+			fig.colorbar(im)
+			
+			ax.xaxis.set_ticks([])
+			ax.yaxis.set(ticks=np.arange(0.5, 12), ticklabels=['phi1 L1', 'L2', 'L3', 'L4', 'theta L1', 'L2', 'L3', 'L4', 'phi2 L1', 'L2', 'L3', 'L4'][::-1])
+			
+			plt.title("\n{}".format(time), fontsize=16)
+			plt.suptitle(r'{w} current ($\mu A$) attenuation={a}'.format(w=wire_strs[wire], a=attenuation))
+			
+			plt.tight_layout()
+			filename = 'data/{s}/scan{s}2d2{w}att{a}.png'.format(s=scan, w=wire_strs[wire], a=attenuation_i)
+			print("Saving {}".format(filename))
+			plt.savefig(filename, bbox_inches='tight')
+			plt.clf()
+			
+		# plot 3: y=superlayer x layer, x=wire
+		if 1:
+			fig, ax = plt.subplots()
+			fig.set_size_inches(6.0, 6.0)
+			im = ax.pcolor(values[:,:,:2].reshape((-1,2))[::-1], cmap='YlOrBr', edgecolor='black')
+			fig.colorbar(im)
+			
+			ax.xaxis.set(ticks=np.arange(0.5, 2), ticklabels=wire_strs[:2])
+			ax.yaxis.set(ticks=np.arange(0.5, 12), ticklabels=['phi1 L1', 'L2', 'L3', 'L4', 'theta L1', 'L2', 'L3', 'L4', 'phi2 L1', 'L2', 'L3', 'L4'][::-1])
+					
+			plt.title("\n{}".format(time), fontsize=16)
+			plt.suptitle(r'Current ($\mu A$) attenuation={a}'.format(a=attenuation))
+			
+			plt.tight_layout()
+			filename = 'data/{s}/scan{s}2d3att{a}.png'.format(s=scan, w=wire_strs[wire], a=attenuation_i)
+			print("Saving {}".format(filename))
+			plt.savefig(filename, bbox_inches='tight')
+			plt.clf()
